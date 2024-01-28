9136926c828f1574810cc8716cf11659331c15b2
amiiirchik <ragimovamir@yandex.ru>
2024-01-29 00:08:43 +0200

    level changes and collision fixes (death from collision with platform)

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -7,16 +7,16 @@ public class PlayerCollision : MonoBehaviour
 
     private void Update()
     {
-        RaycastHit2D rayTop = Physics2D.Raycast(transform.position - new Vector3(0.55f, 0.2f, 0), Vector2.right, 1.1f, layerMask);
-        RaycastHit2D rayBottom = Physics2D.Raycast(transform.position - new Vector3(0.55f, -0.2f, 0), Vector2.right, 1.1f, layerMask);
+        RaycastHit2D rayTop = Physics2D.Raycast(transform.position - new Vector3(0.52f, 0.2f, 0), Vector2.right, 1.04f, layerMask);
+        RaycastHit2D rayBottom = Physics2D.Raycast(transform.position - new Vector3(0.52f, -0.2f, 0), Vector2.right, 1.04f, layerMask);
 
         if (rayTop.collider != null || rayBottom.collider != null)
         {
             DeathHandler.DieFromPlatform();
         }
 
-        Debug.DrawRay(transform.position - new Vector3(0.55f, 0.2f, 0), Vector2.right * 1.1f, Color.red);
-        Debug.DrawRay(transform.position - new Vector3(0.55f, -0.2f, 0), Vector2.right * 1.1f, Color.red);
+        Debug.DrawRay(transform.position - new Vector3(0.52f, 0.2f, 0), Vector2.right * 1.04f, Color.red);
+        Debug.DrawRay(transform.position - new Vector3(0.52f, -0.2f, 0), Vector2.right * 1.04f, Color.red);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
